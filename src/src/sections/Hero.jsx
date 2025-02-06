dc27b0bed670706eb9bf63d994fd923a1834a470
Esdras Jonathan <genjakeyellowstone@gmail.com>
2025-02-06 23:55:28 +0000

    Mise en place responsive 3d room

--- src/src/sections/Hero.jsx
+++ src/src/sections/Hero.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'; 
 import { Suspense } from 'react';
 import { PerspectiveCamera } from '@react-three/drei'; 
-import { AmbientLight } from 'three';
+import { AmbientLight, DirectionalLight } from 'three';
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
+import {Leva, useControls} from "leva"
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from '../constants';
 
 const Hero = () => {
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth : 768, maxWidth : 1024 });
+  
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+
+
   return (
     <section className='relative min-h-screen w-full flex flex-col'>
         <div className='w-full max-auto flex flex-col mt-20 c-space gap-3'>
@@ -19,18 +31,29 @@ const Hero = () => {
         </div>
 
         <div className='w-full h-full absolute inset-0'>
+          {/**<Leva /> */}
           <Canvas className="w-full h-full">
 
             <Suspense fallback={<CanvasLoader />} >
 
-            <PerspectiveCamera makeDefault position={[0,0,30]} />
+            <PerspectiveCamera makeDefault position={[0,0,20]} />
               
-            <HackerRoom scale={0.05} position={[0,0,0]} rotation={[0, -Math.PI/2, 0]} />
+            <HackerRoom 
+            //scale={0.07} 
+            //position={[0,0,0]} 
+            //rotation={[0, 280, 0]} 
+            position={sizes.deskPosition}
+            rotation={[0,-Math.PI,0]} 
+            scale={sizes.deskScale} 
+            />
+            
             
             <ambientLight intensity={1} />
 
+            <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+
             </Suspense>
-          </Canvas>
+          </Canvas> 
         </div>
 
     </section>
