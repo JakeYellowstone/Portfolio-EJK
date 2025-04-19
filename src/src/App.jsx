6e718b5cc7ce9c5a59b727044fc590dba338d859
Esdras Jonathan <genjakeyellowstone@gmail.com>
2025-04-19 12:25:28 +0000

    Scrool down expérience & world globe

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,63 +1,92 @@
+/*================== IMPORT REACT ==================*/
+import { useEffect, useRef, useState } from "react";
+
 /*================== IMPORT MOTION ==================*/
 import { motion } from "framer-motion";
-import AnimationCards from './sections/AnimationCards';
-import FadeScrollSection from './components/FadeScrollSection';
+import AnimationCards from "./sections/AnimationCards";
+import FadeScrollSection from "./components/FadeScrollSection";
 
 /*================== IMPORT COMPOSANT ==================*/
-import About from './sections/About'
-import Projects from './sections/Projects'
-import Contact from './sections/Contact'
-import Footer from './sections/Footer'
-import Experiences from './sections/Experiences'
-import Preloader from './components/Preloader'
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import Experiences from "./sections/Experiences";
+import Preloader from "./components/Preloader";
 
 const App = () => {
+    const [showScrollIndicator, setShowScrollIndicator] = useState(true);
+    const footerRef = useRef(null);
 
-  return (
-    <main className='w-full h-screen'>
-      {<Preloader />}
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setShowScrollIndicator(!entry.isIntersecting);
+            },
+            {
+                root: null,
+                threshold: 0.5, // dès que 10% du footer est visible
+            }
+        );
 
-      <FadeScrollSection>
-        <AnimationCards />  
-      </FadeScrollSection>
+        if (footerRef.current) {
+            observer.observe(footerRef.current);
+        }
 
-      <FadeScrollSection>
-        <About />  
-      </FadeScrollSection>
+        return () => {
+            if (footerRef.current) {
+                observer.unobserve(footerRef.current);
+            }
+        };
+    }, []);
 
-      <FadeScrollSection>
-        <Projects />
-      </FadeScrollSection>
+    return (
+        <main className="w-full h-screen">
+            {<Preloader />}
 
-        <Experiences />
+            <FadeScrollSection>
+                <AnimationCards />
+            </FadeScrollSection>
 
-      <FadeScrollSection>
-        <Contact />  
-      </FadeScrollSection>
-      
-      <Footer />
+            <FadeScrollSection>
+                <About />
+            </FadeScrollSection>
 
-      <div className='fixed xs:bottom-10 bottom-15 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary border-white flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-[#915EFF] mb-1'
-            />
-           
-          </div>
-        </a>
-      </div>
+            <FadeScrollSection>
+                <Projects />
+            </FadeScrollSection>
 
-    </main>
-  )
-}
+            <Experiences />
 
-export default App
+            <FadeScrollSection>
+                <Contact />
+            </FadeScrollSection>
+
+            <div ref={footerRef}>
+                <Footer />
+            </div>
+
+            {showScrollIndicator && (
+                <div className="fixed xs:bottom-10 bottom-15 w-full flex justify-center items-center">
+                    <a href="#about">
+                        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary border-white flex justify-center items-start p-2">
+                            <motion.div
+                                animate={{
+                                    y: [0, 24, 0],
+                                }}
+                                transition={{
+                                    duration: 1.5,
+                                    repeat: Infinity,
+                                    repeatType: "loop",
+                                }}
+                                className="w-3 h-3 rounded-full bg-[#915EFF] mb-1"
+                            />
+                        </div>
+                    </a>
+                </div>
+            )}
+        </main>
+    );
+};
+
+export default App;
--- src/src/components/Contacts.jsx
+++ src/src/components/Contacts.jsx
@@ -1,5 +1,6 @@
 /*================== IMPORT REACT ==================*/
 import React, { useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 /*================== IMPORT MOTION ==================*/
 import { motion } from "framer-motion";
@@ -13,131 +14,130 @@ import { styles } from "../style";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "./hoc";
 
-
 const Contacts = () => {
-  const formRef = useRef();
-  const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState(
-    {
-      name : '',
-      email : '',
-      message : '',
-    }
-  );
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const formRef = useRef();
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
 
-  const handleChange = ({target : {name, value}}) => {
-    setForm({...form, [name] : value})
-  };
+    const handleChange = ({ target: { name, value } }) => {
+        setForm({ ...form, [name]: value });
+    };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
+        try {
+            await emailjs.send(
+                "service_embth4p",
+                "template_y779de5",
+                {
+                    from_name: form.name,
+                    to_name: "Esdras Jonathan",
+                    from_email: form.email,
+                    to_email: "esdraskj@gmail.com",
+                    message: form.message,
+                },
+                "k_TBNgXnxxqVAM1eD"
+            );
 
-    try {
+            setLoading(false);
+            alert("Votre message a bien été envoyé !");
 
-      await emailjs.send(
-        'service_embth4p', 
-        'template_y779de5',
-        {
-          from_name : form.name,
-          to_name : 'Esdras Jonathan',
-          from_email : form.email,
-          to_email : 'esdraskj@gmail.com',
-          message : form.message
-        },
-        'k_TBNgXnxxqVAM1eD'
-      )
+            setForm({
+                name: "",
+                email: "",
+                message: "",
+            });
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
 
-      setLoading(false); 
-      alert("Votre message a bien été envoyé !");
+            alert("Quelque chose s'est mal passé !");
+        }
+    };
 
-      setForm({
-        name : '',
-        email : '',
-        message : '',
-      })
-      
-    } catch (error) {
-
-      setLoading(false);
-      console.log(error);
-
-      alert("Quelque chose s'est mal passé !")
-      
-    }
-
-  };
-
-  return (
-    <div
-      className={`flex xl:flex-row flex-col-reverse gap-10 overflow-hidden `}
-    >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 rounded-2xl'
-      >
-        <p className={styles.sectionSubText}><span className='text-[#915EFF]'>Changeons le monde ensemble</span></p>
-        <h3 className={styles.sectionHeadText}>Contactez moi !</h3>
-
-        <form ref={formRef} onSubmit={handleSubmit} className='flex flex-col space-y-7'>
-                  <label className='space-y-3'>
-                    <span className='field-label'>
-                      Nom et prénoms
+    return (
+        <div
+            className={`flex xl:flex-row flex-col-reverse gap-10 overflow-hidden `}
+        >
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className="flex-[0.75] bg-black-100 rounded-2xl"
+            >
+                <p className={styles.sectionSubText}>
+                    <span className="text-[#915EFF]">
+                        Changeons le monde ensemble
                     </span>
-                    <input 
-                      type="text"
-                      name='name'
-                      value={form.name}
-                      onChange={handleChange}
-                      required
-                      className='field-input mt-2'
-                      placeholder='Remplissez ici' 
-                    />
-                  </label>
-                  <label className='space-y-3'>
-                    <span className='field-label'>
-                      Votre Email
-                    </span>
-                    <input 
-                      type="email"
-                      name='email'
-                      value={form.email}
-                      onChange={handleChange}
-                      required
-                      className='field-input mt-2'
-                      placeholder='Votre email' 
-                    />
-                  </label>
-                  <label className='space-y-3'>
-                    <span className='field-label'>
-                      Votre message
-                    </span>
-                    <textarea 
-                      name='message'
-                      value={form.message}
-                      onChange={handleChange}
-                      required
-                      rows={5}
-                      className='field-input mt-2'
-                      placeholder="Votre message ici..." 
-                    />
-                  </label>
+                </p>
+                <h3 className={styles.sectionHeadText}>Contactez moi !</h3>
 
-                  <button className='field-btn' type='submit' disabled={loading}>
-                    {loading ? 'Envoi en cours...' : "Envoyez message"}
-                  </button>
+                <form
+                    ref={formRef}
+                    onSubmit={handleSubmit}
+                    className="flex flex-col space-y-7"
+                >
+                    <label className="space-y-3">
+                        <span className="field-label">Nom et prénoms</span>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            required
+                            className="field-input mt-2"
+                            placeholder="Remplissez ici"
+                        />
+                    </label>
+                    <label className="space-y-3">
+                        <span className="field-label">Votre Email</span>
+                        <input
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            required
+                            className="field-input mt-2"
+                            placeholder="Votre email"
+                        />
+                    </label>
+                    <label className="space-y-3">
+                        <span className="field-label">Votre message</span>
+                        <textarea
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            required
+                            rows={5}
+                            className="field-input mt-2"
+                            placeholder="Votre message ici..."
+                        />
+                    </label>
+
+                    <button
+                        className="field-btn"
+                        type="submit"
+                        disabled={loading}
+                    >
+                        {loading ? "Envoi en cours..." : "Envoyez message"}
+                    </button>
                 </form>
-      </motion.div>
+            </motion.div>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <EarthCanvas />
-      </motion.div>
-    </div>
-  );
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className={`xl:flex-1 xl:h-auto md:h-[550px] h-[350px] ${isMobile ? 'pointer-events-none' : ''}`}
+            >
+                <EarthCanvas />
+            </motion.div>
+        </div>
+    );
 };
 
 export default SectionWrapper(Contacts, "contact");
